e511fdd2e4726b7af938fd6b9f7333da3ad5a94d
Elliot Humphrey <evhumphrey@users.noreply.github.com>
2019-02-18 16:38:07 -0800

    Trim controllers and update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  # get '/users', to: 'users#index'
-  # get '/users/:id', to: 'users#show'
-  # post '/users/', to: 'users#create'
-  # patch '/users/:id', to: 'users#update'
-  # delete '/users/:id', to: 'users#destroy'
-
-  # resources :users, only: [:new, :edit] # opposite of except
-  resources :users do
-    # did create instead of index
-
-    resources :chirps, only: [:create]
+  namespace :api, defaults: { format: :json } do
+    resources :chirps, only: [:index, :create]
   end
-  resource :session, only: [:new, :create, :destroy]
 
-  resources :chirps, only: [:show]
-
+  root to: 'static_pages#root'
 end
